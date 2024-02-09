0619149297b3e05e4b7d0e54ef823a46b4f0be5e
Vijay Singh <78549179+itsvijaysingh@users.noreply.github.com>
2024-02-09 17:50:26 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,20 @@ document.querySelectorAll('button.option').forEach(button => {
     button.addEventListener('click', () => {
         let valueContainer = document.getElementById('value');
         let option = button.value;
-        if(option === '='){
-            // evaluate the value
+        if (option === '=') {
+            // Evaluate the value
             let values = valueContainer.value;
             let result = eval(values);
             valueContainer.value = `${result}`;
-        }else if(option === 'C'){
-            //clear current inputs
+        } else if (option === 'C') {
+            // Clear current inputs
             valueContainer.value = '';
-        }else if(option.value = 'X'){
-            //remove last value
-            valueContainer.value.slice(0, -1);
-        }else{
-            //add values
+        } else if (option === 'X') {
+            // Remove last value
+            valueContainer.value = valueContainer.value.slice(0, -1);
+        } else {
+            // Add values
             valueContainer.value += `${option}`;
         }
     });
-})
+});
